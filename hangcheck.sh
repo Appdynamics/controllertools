a6cdaf871891deb0a709f120ea44a1ec8cfa77f9
cmayer <cmayer@appdynamics.com>
2018-04-28 14:42:25 -0700

    comments and loop bug fix

--- hangcheck.sh
+++ hangcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# $Id: hangcheck.sh 1.1 2018-04-28 14:23:28 cmayer
+# $Id: hangcheck.sh 1.2 2018-04-28 14:42:23 cmayer
 #
 # this is intended to be a process that pings a local controller port
 # if it times out often enough, generate a thread dump.
@@ -14,26 +14,57 @@ CONTROLLER_ROOT=$(ps -ef | \
 
 LOGFILE=$CONTROLLER_ROOT/logs/hangcheck.log
 
+#
+# ping this port
+#
 PROTOCOL=http
 PORT=8293
+URL=$PROTOCOL://localhost:$PORT/controller/rest/serverstatus
 
+#
+# every $INTERVAL seconds
+#
 INTERVAL=15
+
+#
+# $FAILURES in a row causes dump
+#
 FAILURES=3
 
-URL=$PROTOCOL://localhost:$PORT/controller/rest/serverstatus
-LASTDUMP=0
+#
+# we only make 1 dump every $DUMPFREQ seconds
+#
 DUMPFREQ=$((30*60))
+
+#
+# keep $KEEP logs around; toss the rest
+#
 KEEP=5
 
+#
+# where to put the thread dumps
+#
 DUMPDIR=$CONTROLLER_ROOT/logs
+
+#
+# not strictly the last dump - the last dump from this process
+#
+lastdump=0
+
+#
+# how many times our ping failed this iteration
+#
 losecount=0
 
+#
+# cleaner than using a signal, since we can put the output where we want it
+#
 DUMPCMD="$CONTROLLER_ROOT/appserver/glassfish/bin/asadmin --user=admin --passwordfile=$CONTROLLER_ROOT/.passwordfile \
 	generate-jvm-report --type=thread"
 
-while [ 1 ]; do
+while true ; do
 	NOW=$(date +%s)
-	NEXT=`expr $NOW + $INTERVAL`
+	NEXT=$((NOW + INTERVAL))
 
 	#
 	# ping our url
@@ -52,14 +83,12 @@ while [ 1 ]; do
 			#	
 			# we can't dump too often
 			#
-			if [ $(date +%s) -gt $((LASTDUMP + DUMPFREQ)) ] ; then
+			if [ $(date +%s) -gt $((lastdump + DUMPFREQ)) ] ; then
 				pid=$(pgrep -f $CONTROLLER_ROOT/appserver/glassfish/domains/domain1/config)
 
 				#
 				# if there's already a dumpfile for this pid, save it.
 				# let's keep the last $KEEP saved files
-				# XX - this code will leak thread dumps if we restart a lot.
-				#      there could be a lot of hotspot_pidxxxxx.log files
 				#
 				dumpfile=$DUMPDIR/thread_dump.log
 				if [ -f $dumpfile ] ; then
@@ -67,10 +96,12 @@ while [ 1 ]; do
 					rm -f $(ls -1t $DUMPDIR/thread_dump.* 2>/dev/null | tail +$((KEEP+1)))
 				fi
 				$DUMPCMD >$dumpfile
-				LASTDUMP=$(date +%s)
+				lastdump=$(date +%s)
 			fi
 			losecount=0
 		fi
+	else
+		losecount=0
 	fi
 
 	NOW=$(date +%s)
