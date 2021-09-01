8f76f25b9d7ddbbab046c57ea36d6d85893a530c
Rob Navarro <16707665+rnav1234@users.noreply.github.com>
2021-09-01 15:24:43 -0700

    initial commit from remote repo

---
+++ light_weight_server_monitor/flask-monxv/constants.py
@@ -0,0 +1,2 @@
+BASH = '/usr/local/bin/bash'
+LOAD_SCRIPT = '/Users/robnav/github/controllertools/light_weight_server_monitor/dataconv/load_all.sh'
---
+++ light_weight_server_monitor/flask-monxv/run_cmd.py
@@ -0,0 +1,31 @@
+# from: https://github.com/fabianlee/blogcode/blob/master/python/runProcessWithLiveOutput.py
+
+import sys
+import subprocess
+
+# initial outline from: https://github.com/fabianlee/blogcode/blob/master/python/runProcessWithLiveOutput.py
+# Idea is to have a long running (some minutes) script return each line of progress as it is emitted.
+# Ideal Python feature is not 'return' but yield that slickly resumes thereafter at next function call :-)
+# This permits a caller like:
+#   for r in run_cmd(xyz): process(r)
+
+def run_cmd(command):
+	try:
+		process = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+	except:
+		# print("ERROR {} while running {}".format(sys.exc_info()[1], command))
+		str = "ERROR: %s while running: %s" % (sys.exc_info()[1], command)
+		print(str)
+		return(str)
+	output = process.stdout.readline().rstrip()			# remove RHS whitespace including newline(s)
+	while len(output) > 0:
+		yield output
+		output = process.stdout.readline().rstrip()
+
+
+def run_cmd3(command, shellType=False, stdoutType=subprocess.PIPE):
+	yield "ERROR: /Users/robnav/github/controllertools/light_weight_server_monitor/dataconv/load_all.sh (f=main,l=322) failed: no valid -d directories"
+	yield "Usage: load_all.sh -d <data dir1>,<d2>,<d3> 	# load all monitor data therein - comma separated directories"
+	yield "[-e]						# empty openTSDB of data for test1.1m.avg"
+	yield "[-m	<mon1>,<mon2>]				# chosen monitors to load - comma separated"
+	yield "[-c	<dataconv directory>]			# where the conversion tools live"
